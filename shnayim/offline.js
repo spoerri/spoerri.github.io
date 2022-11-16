3cfc224e0857b6d94640a8dafa5cbdad1e252294
Joshua Spoerri <spoerri@gmail.com>
2022-11-16 11:37:16 -0500

    fix offline cache

--- shnayim/offline.js
+++ shnayim/offline.js
@@ -2,13 +2,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('shmos').then(function(cache) {
      return cache.addAll([
-       '/shnayim',
-       '/shnayim/index.html',
-       '/shnayim/1.html',
-       '/shnayim/2.html',
-       '/shnayim/3.html',
-       '/shnayim/4.html',
-       '/shnayim/5.html'
+       '/shnayim/shnayim.html'
      ]);
    })
  );
